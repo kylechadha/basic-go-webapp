671a7fe26ed5699887ca116a8e6a7a7719e9a836
Kyle Chadha <kyle.chadha@gmail.com>
2015-01-29 15:02:06 -0500

    Continue to define server.go; Define about and index views

--- server.go
+++ server.go
@@ -3,27 +3,41 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
+const STATIC_URL string = "/static/"
+const STATIC_ROOT string = "static/"
+
+// This is the struct made available to our templates
+type Context struct {
+	Title  string
+	Static string
+}
+
 func Home(w http.ResponseWriter, req *http.Request) {
+	context := Context{Title: "Welcome! Home Page"}
 	render(w, "index.html")
 }
 
 func About() {
+	context := Context{Title: "About Page"}
 	render(w, "about.html")
 }
 
-func render(w http.ResponseWriter, tmpl string) {
-	tmpl = fmt.Sprintf("template/%s", tmpl)
+func render(w http.ResponseWriter, tmpl string, context Context) {
+	context.Static = STATIC_URL
+	tmpl_list = []string{"templates/layout.html", fmt.Sprintf("template/%s", tmpl)}
 
-	t, err := template.ParseFiles(tmpl)
+	t, err := template.ParseFiles(tmpl_list...)
 	if err != nil {
 		log.Print("Template parsing error: ", err)
 	}
 
-	err = t.Execute(w, "")
+	err = t.Execute(w, context)
 	if err != nil {
 		log.Print("Template execution erro: ", err)
 	}
@@ -39,11 +53,15 @@ func StaticHandler(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
+
 	http.NotFound(w, req)
 }
 
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about/", About)
+	http.HandleFunc(STATIC_URL, StaticHandler)
+
 	err := http.ListenAndServe(":8000", nil)
 
 	if err != nil {
