55afd6c6bf8e050c7c971828e4f835659a4d2f36
Kyle Chadha <kyle.chadha@gmail.com>
2015-01-29 14:26:20 -0500

    Define render function

--- server.go
+++ server.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hellooo Golang!")
+
+}
+
+func About() {
+
+}
+
+func render(w http.ResponseWriter, tmpl string) {
+	tmpl = fmt.Sprintf("template/%s", tmpl)
+
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		log.Print("Template parsing error: ", err)
+	}
+
+	err = t.Execute(w, "")
+	if err != nil {
+		log.Print("Template execution erro: ", err)
+	}
 }
 
 func main() {
